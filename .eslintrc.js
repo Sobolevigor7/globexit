c03ef48ffc5673451607fd6fa1b8ece119bf8567
Igor Sobolev <86300668+Sobolevigor7@users.noreply.github.com>
2022-10-15 19:14:04 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb-base'],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'no-alert': 0,
+    'no-param-reassign': [2, { props: false }],
+    'no-plusplus': 0,
+    'no-iterator': 0,
+    'no-restricted-syntax': [2, 'WithStatement'],
+    'func-style': 0,
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    codeFrame: false,
+    ecmaVersion: 2022,
+  },
+};
